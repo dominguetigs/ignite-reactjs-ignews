9834001c8660e9bf4388ecd410465dc2edff0e91
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-07-06 12:26:46 -0300

    feat: update logic on subscribe api root.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,7 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import { Casefold, Collection, Get, Index, Match, Ref, Update } from 'faunadb';
 
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -9,13 +20,29 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       user: { email },
     } = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email,
-      // metadata
-    });
+    const user = await fauna.query<User>(Get(Match(Index('user_by_email'), Casefold(email))));
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email,
+        // metadata
+      });
+
+      await fauna.query(
+        Update(Ref(Collection('users'), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
