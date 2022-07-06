42d92f8e04c024a62de9976188492c1f35c0ba00
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-07-06 11:36:17 -0300

    feat: implement logic to create and get user from faunaDB.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { Casefold, Collection, Create, Exists, Get, If, Index, Match, Not } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -13,4 +17,22 @@ export default NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user: { email } }) {
+      const matchByIndex = Match(Index('user_by_email'), Casefold(email));
+
+      try {
+        await fauna.query(
+          If(
+            Not(Exists(matchByIndex)),
+            Create(Collection('users'), { data: { email } }),
+            Get(matchByIndex)
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
