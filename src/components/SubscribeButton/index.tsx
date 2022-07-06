f6863387496835fedc126dbca4add71581737535
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2022-07-06 12:16:55 -0300

    feat: create stripe-js service.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 import styles from './styles.module.scss';
 
@@ -9,13 +11,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { status } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (status === 'unauthenticated') {
       signIn('github');
       return;
     }
 
     // Create stripe checkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   return (
