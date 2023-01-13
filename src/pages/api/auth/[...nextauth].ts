e77c16955c99e2a4856fe0a3f7c97f8e4c9be09d
Gustavo Domingueti <gustavo.domingueti@skeelo.app>
2023-01-12 22:21:16 -0300

    feat: update nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { Casefold, Collection, Create, Exists, Get, If, Index, Intersection, Mat
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-import { fauna } from '../../services/fauna';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
